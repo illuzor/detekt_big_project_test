b8f13b8b6951e36814379ac69b9aff2e2b60c2cd
Artem Zanin <illuzor@gmail.com>
2023-07-30 18:34:38 +0600

    files added

---
+++ big_project/module95/build/BG.kts
@@ -0,0 +1,26 @@
+package com.illuzor.dbpt
+
+plugins {
+    id("java")
+    kotlin("jvm")
+}
+
+group = "com.illuzor.dbpt"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(8)
+}
